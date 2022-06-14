79013f9207251b6d5ddcfd6ffabc5986a1b67a53
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2022-06-14 11:44:30 +0200

    Add active class to navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,19 +2,22 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../assets/images/logo.png';
 
-const Navbar = () => (
-
-  <nav>
-    <img src={logo} alt="logo" />
-    <div>
-      <ul>
-        <li><NavLink to="./">Rockets</NavLink></li>
-        <li><NavLink to="./missions">Missions</NavLink></li>
-        <li><NavLink to="./myprofile">My Profile</NavLink></li>
-      </ul>
-    </div>
-  </nav>
-
-);
+const Navbar = () => {
+  const navLinks = [
+    { name: 'Rockets', path: '/', id: '1' },
+    { name: 'Missions', path: '/missions', id: '2' },
+    { name: 'My Profile', path: '/myprofile', id: '3' },
+  ];
+  return (
+    <nav>
+      <img src={logo} alt="logo" />
+      <div>
+        <ul>
+          {navLinks.map((el) => <li key={el.id}><NavLink to={el.path} className={({ isActive }) => (isActive ? 'active-style' : 'none')}>{el.name}</NavLink></li>)}
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,5 +1,7 @@
 const hello1 = 'hello';
 const rockets = [];
+
+export const handleHello = () => ({ type: hello1 });
 const rocketsReducer = (state = rockets, action) => {
   switch (action.type) {
     case hello1:
