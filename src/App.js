8e1f5d48a051cb13b2606d6ee8c883a860499336
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2022-06-14 11:25:43 +0200

    Setup routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Rockets from './components/Rockets';
+import Missions from './components/Missions';
+import MyProfile from './components/MyProfile';
 
 function App() {
   return (
-    <div className="App">
+
+    <BrowserRouter>
       <Navbar />
-    </div>
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/myprofile" element={<MyProfile />} />
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from '../assets/images/logo.png';
 
 const Navbar = () => (
-  <div>Navbar</div>
+
+  <nav>
+    <img src={logo} alt="logo" />
+    <div>
+      <ul>
+        <li><NavLink to="./">Rockets</NavLink></li>
+        <li><NavLink to="./missions">Missions</NavLink></li>
+        <li><NavLink to="./myprofile">My Profile</NavLink></li>
+      </ul>
+    </div>
+  </nav>
+
 );
 
 export default Navbar;
