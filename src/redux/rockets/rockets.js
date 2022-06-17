503a58657abd820096611830d89b1956c5391009
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2022-06-17 10:35:30 +0200

    fetch rockets data from API

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,13 +1,19 @@
-const hello1 = 'hello';
+import axios from 'axios';
+
+const FETCH_ROCKETS = 'FETCH_ROCKETS';
+
 const rockets = [];
 
-export const handleHello = () => ({ type: hello1 });
+export const FetchRocketsHandler = () => async (dispatch) => {
+  const response = await axios.get('https://api.spacexdata.com/v3/rockets');
+  return dispatch({ type: FETCH_ROCKETS, payload: response.data });
+};
+
 const rocketsReducer = (state = rockets, action) => {
   switch (action.type) {
-    case hello1:
-      return [
-        ...state, 'hello',
-      ];
+    case FETCH_ROCKETS:
+      return action.payload.map((rocket) => ({ ...rocket, active: false }));
+
     default: return state;
   }
 };
